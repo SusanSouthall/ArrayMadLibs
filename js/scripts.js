7a0736a5f9d3bfb843a2d233dbe0b558a14e3d64
julia sheremet and Susan Southall <susan@>
2018-03-26 11:55:01 -0700

    clean code and refactor using array and forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
 $(document).ready(function(){
   $("form#mad-lib-form").submit(function(event){
     event.preventDefault();
-    var userinput1 = $("input#input-1").val();
-    var userinput2 = $("input#input-2").val();
-    var userinput3 = $("input#input-3").val();
-    var userinput4 = $("input#input-4").val();
-    var userinput5 = $("input#input-5").val();
-    var userinput6 = $("input#input-6").val();
-    var userinput7 = $("input#input-7").val();
-    var userinput8 = $("input#input-8").val();
-    var userinput9 = $("input#input-9").val();
 
-    $(".output-1").text(userinput1);
-    $(".output-2").text(userinput2);
-    $(".output-3").text(userinput3);
-    $(".output-4").text(userinput4);
-    $(".output-5").text(userinput5);
-    $(".output-6").text(userinput6);
-    $(".output-7").text(userinput7);
-    $(".output-8").text(userinput8);
-    $(".output-9").text(userinput9);
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun2", "input7", "input8", "input9"];
 
-    $(".mad-lib-story").show();
-    $("#mad-lib-form").hide();
+    blanks.forEach(function(blank){
+      var inputFromUser = $("input#" + blank).val();
+      $("." + blank).text(inputFromUser);
+      debugger;
+    });
+
+
+
+    // var userinput1 = $("input#input1").val();
+    // var userinput2 = $("input#input2").val();
+    // var userinput3 = $("input#input3").val();
+    // var userinput4 = $("input#input4").val();
+    // var userinput5 = $("input#input5").val();
+    // var userinput6 = $("input#input6").val();
+    // var userinput7 = $("input#input7").val();
+    // var userinput8 = $("input#input8").val();
+    // var userinput9 = $("input#input9").val();
+    //
+    // $(".output1").text(userinput1);
+    // $(".output2").text(userinput2);
+    // $(".output3").text(userinput3);
+    // $(".output4").text(userinput4);
+    // $(".output5").text(userinput5);
+    // $(".output6").text(userinput6);
+    // $(".output7").text(userinput7);
+    // $(".output8").text(userinput8);
+    // $(".output9").text(userinput9);
+
+     $(".mad-lib-story").show();
+    // $("#mad-lib-form").hide();
   });
 });
